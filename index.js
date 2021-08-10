db787a182cfdcc61794a5bc9501644250fafbc02
Atul Patil <atulpatil539@gmail.com>
2021-08-10 17:39:15 +0530

    feat: added

---
+++ index.js
@@ -0,0 +1,54 @@
+var billAmount = document.querySelector("#bill-amount");
+var cashGiven = document.querySelector("#cash-given");
+var checkButton = document.querySelector("#check-button");
+var message = document.querySelector("#error-message");
+var noOfNotes = document.querySelectorAll(".no-of-notes");
+
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1] ;
+ 
+checkButton.addEventListener("click", function validateBillAndCashAmount(){
+    hideMessage();
+    if (billAmount.value > 0 ){
+        if (cashGiven.value >= billAmount.value){
+            const amountToBeReturned = cashGiven.value - billAmount.value
+            calculateChange(amountToBeReturned); 
+        } else {
+             showMessage("Cash is less than Bill Amount, Please give full Bill Amount!!!")
+        }
+        
+
+    } else{
+      showMessage("Invalid Bill Amount!")
+    }
+} 
+)
+
+function calculateChange(amountToBeReturned){
+  for (i=0 ; i < availableNotes.length ; i++ ){
+    const numberOfNotes = Math.trunc(
+        amountToBeReturned/availableNotes[i]
+    );
+    amountToBeReturned = amountToBeReturned % availableNotes[i];
+   noOfNotes[i].innerText = numberOfNotes;
+  }
+}
+
+
+
+
+
+
+function hideMessage(){
+    message.style.display= "none";
+}
+
+function showMessage(msg){
+    message.style.display ="block";
+    message.innerText = msg
+}
+
+
+
+
+
